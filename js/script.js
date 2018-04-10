02bf938c34a8e28d02ebd72a2a9dd2c55ef298af
Kowalskihahaha <125242548@qq.com>
2018-04-11 02:32:26 +0800

    基础文件

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('#btn').click(function(){
+	alert('123');
+})
+var xhr = new XMLHttpRequest();
+xhr.open('GET','../data/test.json',true);
+xhr.send(null);
+
+xhr.onload = function(){
+	if(xhr.status === 200){
+		var jsonObject;
+		jsonObject = JSON.parse(xhr.responseType);
+	}
+	document.getElementById('jsonText').innerHTML(jsonObject.project[0].time);
+}
+
