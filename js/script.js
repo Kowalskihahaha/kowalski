9b9eb1a3045c5feb50968c77c582bba183be5974
Kowalskihahaha <125242548@qq.com>
2018-04-11 22:27:01 +0800

    3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = function(){
-	if(xhr.status === 200){
-		resposeObject = JSON.parse(xhr.responseText);
-	
-		var newContent = '';
 
-		newContent = '<div>' + resposeObject.events.length + '</div>';
+$('#btn').click(function(){
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function(){
+		if(xhr.status === 200){
+			resposeObject = JSON.parse(xhr.responseText);
+		
+			var newContent = '';
 	
-		document.getElementById('resText').innerHTML = newContent;
-	}
-};
-xhr.open('GET','data/data.json',true);
-xhr.send(null);
+			newContent = '<div>' + resposeObject.events.length + '</div>';
+		
+			document.getElementById('resText').innerHTML = newContent;
+		}
+	};
+	xhr.open('GET','data/data.json',true);
+	xhr.send(null);	
+})
